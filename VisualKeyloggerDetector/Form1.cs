87cb3f394c9b1ae68e7b7e2204d0b2b8fadb8e0c
Aussteja <bukauskyte.austeja@gmail.com>
2017-12-04 19:07:14 +0100

    we did a lot

--- VisualKeyloggerDetector/Form1.cs
+++ VisualKeyloggerDetector/Form1.cs
@@ -13,16 +13,62 @@ namespace VisualKeyloggerDetector
 {
     public partial class Form1 : Form
     {
+        private Timer m_Timer;
+        private Test m_Test;
+        private int m_KeyCount = 100;
+
         public Form1()
         {
             InitializeComponent();
+
+            m_Timer = new Timer();
+            m_Timer.Interval = 100;
+            m_Timer.Start();
+            m_Timer.Tick += new EventHandler(timer_Tick);
+
+            m_Test = new Test();
+            var processes = Win32_Process.GetAllProcesses();
+            foreach (var item in processes)
+            {
+                var info = new Test.ProgramInfo();
+                info.Name = item.Name;
+                info.Path = item.ExecutablePath;
+                info.WriteCount = item.WriteTransferCount.Value;
+                m_Test.StartPrograms.Add(info);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            return;
             VirtualInput.SendInput('v');
             var processes = Win32_Process.GetAllProcesses();
             Console.WriteLine(processes.Count);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            VirtualInput.SendInput('a');
+            m_KeyCount--;
+            if (m_KeyCount == 0)
+            {
+                m_Timer.Stop();
+
+                finish_Test();
+            }
+        }
+
+        private void finish_Test()
+        {
+            var processes = Win32_Process.GetAllProcesses();
+            foreach (var item in processes)
+            {
+                var info = new Test.ProgramInfo();
+                info.Name = item.Name;
+                info.Path = item.ExecutablePath;
+                info.WriteCount = item.WriteTransferCount.Value;
+                m_Test.EndPrograms.Add(info);
+            }
         }
     }
 }
